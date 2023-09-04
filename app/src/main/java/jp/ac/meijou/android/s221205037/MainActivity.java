a396256c6827321af00dcba49507343b1ba57d9b
First-name Family-name <username@example.com>
2023-09-04 15:18:06 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/meijou/android/s221205037/MainActivity.java
@@ -0,0 +1,14 @@
+package jp.ac.meijou.android.s221205037;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
